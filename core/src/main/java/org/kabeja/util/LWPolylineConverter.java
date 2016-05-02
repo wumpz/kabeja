655bc31c054041987147dad8aaf6111f64d4680e
KyleAdams <kyle.adams@ticketfly.com>
2016-05-02 16:26:04 -0700

    Adjust lwpolyline converter to not add to layers/documents and not remove from layer

--- core/src/main/java/org/kabeja/util/LWPolylineConverter.java
+++ core/src/main/java/org/kabeja/util/LWPolylineConverter.java
@@ -9,14 +9,10 @@ public class LWPolylineConverter {
     public static LWPolyline toLWPolyline(Polyline pl) {
         LWPolyline lwp = new LWPolyline();
         lwp.setColor(pl.getColor());
-        lwp.setLayer(pl.getLayer());
-        lwp.setDocument(pl.getDocument());
 
         for (Vertex vertex : pl.getVertices()) {
             lwp.addVertex(new LW2DVertex(vertex.getPoint().getX(), vertex.getPoint().getY()));
         }
-
-        pl.getLayer().removeEntity(pl);
 
         return lwp;
     }
@@ -24,15 +20,11 @@ public class LWPolylineConverter {
     public static LWPolyline toLWPolyline(Line line) {
         LWPolyline lwp = new LWPolyline();
         lwp.setColor(line.getColor());
-        lwp.setLayer(line.getLayer());
-        lwp.setDocument(line.getDocument());
 
         Point3D startPoint = line.getStartPoint();
         Point3D endPoint = line.getEndPoint();
         lwp.addVertex(new LW2DVertex(startPoint.getX(), startPoint.getY()));
         lwp.addVertex(new LW2DVertex(endPoint.getX(), endPoint.getY()));
-
-        line.getLayer().removeEntity(line);
 
         return lwp;
     }
