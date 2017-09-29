9e6b093b19e97b1a368e54a58f6c0d3e3a4fc7be
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2017-09-29 11:36:33 +0200

    add Main in dxf-module for dev

---
+++ blocks/dxf/src/main/java/org/kabeja/dxf/Main.java
@@ -0,0 +1,38 @@
+package org.kabeja.dxf;
+
+import org.kabeja.DraftDocument;
+import org.kabeja.dxf.parser.DXFParserBuilder;
+import org.kabeja.parser.ParseException;
+import org.kabeja.parser.Parser;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.nio.file.Files.newInputStream;
+
+public class Main {
+
+    /*
+     * For developing.
+     */
+    public static void main(String[] args) {
+
+        Path dxf = Paths.get("C:\\Users\\Veronika\\Desktop\\dev.dxf");
+
+        try (BufferedInputStream bis = new BufferedInputStream(newInputStream(dxf))) {
+            Parser parser = DXFParserBuilder.createDefaultParser();
+            Map<String, Object> properties = new HashMap<>();
+            properties.put(DraftDocument.PROPERTY_ENCODING, "UTF-8");
+            DraftDocument draftDocument = parser.parse(bis, properties);
+
+            // do stuff...
+
+        } catch (ParseException | NullPointerException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
