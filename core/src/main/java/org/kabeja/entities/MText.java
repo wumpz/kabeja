0e734001dc8173f5da75a3c92cc177f6eeab4395
Tobias Warneke <t.warneke@gmx.net>
2022-04-26 13:51:02 +0200

    corrected mtext rotation

--- core/src/main/java/org/kabeja/entities/MText.java
+++ core/src/main/java/org/kabeja/entities/MText.java
@@ -1,19 +1,18 @@
-/*******************************************************************************
+/** *****************************************************************************
  * Copyright 2010 Simon Mieth
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- ******************************************************************************/
-
+ ***************************************************************************** */
 package org.kabeja.entities;
 
 import org.kabeja.common.Type;
@@ -22,12 +21,12 @@ import org.kabeja.entities.util.TextParser;
 import org.kabeja.entities.util.Utils;
 import org.kabeja.math.Bounds;
 
-
 /**
  * @author <a href="mailto:simon.mieth@gmx.de>Simon Mieth</a>
  *
  */
 public class MText extends Text {
+
     public static final int ATTACHMENT_TOP_LEFT = 1;
     public static final int ATTACHMENT_TOP_CENTER = 2;
     public static final int ATTACHMENT_TOP_RIGHT = 3;
@@ -41,7 +40,6 @@ public class MText extends Text {
     private double refwidth = 0.0;
     private double refheight = 0.0;
 
-    
     public void setAttachmentPoint(int value) {
         this.attachmentpointLocation = value;
     }
@@ -69,18 +67,12 @@ public class MText extends Text {
     public double getRotation() {
         if (rotation != 0.0) {
             return rotation;
-        } else if ((alignmentPoint.getX() != 0.0) || (alignmentPoint.getY() != 0.0) ||
-                (alignmentPoint.getZ() != 0.0)) {
+        } else if ((alignmentPoint.getX() != 0.0) || (alignmentPoint.getY() != 0.0)
+                   || (alignmentPoint.getZ() != 0.0)) {
             // the align point as direction vector here
-            // calculate the angle between the x-axis and the direction-vector
-            double[] x = { alignmentPoint.getX(), alignmentPoint.getY(), alignmentPoint.getZ() };
-            double v = alignmentPoint.getX() / Utils.vectorValue(x);
-            v = Math.toDegrees(Math.acos(v));
-
-            return v;
+            return Math.toDegrees(Math.atan2(alignmentPoint.getY(), alignmentPoint.getX()));
         }
-
-        // same as 0.0
+        
         return rotation;
     }
 
@@ -108,7 +100,6 @@ public class MText extends Text {
         return true;
     }
 
-
     public Bounds getBounds() {
         Bounds bounds = new Bounds();
         int l = this.textDoc.getMaximumLineLength();
@@ -124,73 +115,73 @@ public class MText extends Text {
             h *= this.textDoc.getLineCount();
 
             switch (this.attachmentpointLocation) {
-            case ATTACHMENT_BOTTOM_CENTER:
-                bounds.addToBounds(this.p.getX() + (w / 2), this.p.getY() + h,
-                    p.getZ());
-                bounds.addToBounds(this.p.getX() - (w / 2), this.p.getY(),
-                    p.getZ());
+                case ATTACHMENT_BOTTOM_CENTER:
+                    bounds.addToBounds(this.p.getX() + (w / 2), this.p.getY() + h,
+                                       p.getZ());
+                    bounds.addToBounds(this.p.getX() - (w / 2), this.p.getY(),
+                                       p.getZ());
 
-                break;
+                    break;
 
-            case ATTACHMENT_BOTTOM_LEFT:
-                bounds.addToBounds(this.p.getX() + w, this.p.getY() + h,
-                    p.getZ());
-                bounds.addToBounds(this.p.getX(), this.p.getY(), p.getZ());
+                case ATTACHMENT_BOTTOM_LEFT:
+                    bounds.addToBounds(this.p.getX() + w, this.p.getY() + h,
+                                       p.getZ());
+                    bounds.addToBounds(this.p.getX(), this.p.getY(), p.getZ());
 
-                break;
+                    break;
 
-            case ATTACHMENT_BOTTOM_RIGHT:
-                bounds.addToBounds(this.p.getX() - w, this.p.getY() + h,
-                    p.getZ());
-                bounds.addToBounds(this.p.getX(), this.p.getY(), p.getZ());
+                case ATTACHMENT_BOTTOM_RIGHT:
+                    bounds.addToBounds(this.p.getX() - w, this.p.getY() + h,
+                                       p.getZ());
+                    bounds.addToBounds(this.p.getX(), this.p.getY(), p.getZ());
 
-                break;
+                    break;
 
-            case ATTACHMENT_MIDDLE_CENTER:
-                bounds.addToBounds(this.p.getX() + (w / 2),
-                    this.p.getY() + (h / 2), p.getZ());
-                bounds.addToBounds(this.p.getX() - (w / 2),
-                    this.p.getY() - (h / 2), p.getZ());
+                case ATTACHMENT_MIDDLE_CENTER:
+                    bounds.addToBounds(this.p.getX() + (w / 2),
+                                       this.p.getY() + (h / 2), p.getZ());
+                    bounds.addToBounds(this.p.getX() - (w / 2),
+                                       this.p.getY() - (h / 2), p.getZ());
 
-                break;
+                    break;
 
-            case ATTACHMENT_MIDDLE_LEFT:
-                bounds.addToBounds(this.p.getX(), this.p.getY() + (h / 2),
-                    p.getZ());
-                bounds.addToBounds(this.p.getX() + w, this.p.getY() - (h / 2),
-                    p.getZ());
+                case ATTACHMENT_MIDDLE_LEFT:
+                    bounds.addToBounds(this.p.getX(), this.p.getY() + (h / 2),
+                                       p.getZ());
+                    bounds.addToBounds(this.p.getX() + w, this.p.getY() - (h / 2),
+                                       p.getZ());
 
-                break;
+                    break;
 
-            case ATTACHMENT_MIDDLE_RIGHT:
-                bounds.addToBounds(this.p.getX(), this.p.getY() + (h / 2),
-                    p.getZ());
-                bounds.addToBounds(this.p.getX() - w, this.p.getY() - (h / 2),
-                    p.getZ());
+                case ATTACHMENT_MIDDLE_RIGHT:
+                    bounds.addToBounds(this.p.getX(), this.p.getY() + (h / 2),
+                                       p.getZ());
+                    bounds.addToBounds(this.p.getX() - w, this.p.getY() - (h / 2),
+                                       p.getZ());
 
-                break;
+                    break;
 
-            case ATTACHMENT_TOP_LEFT:
-                bounds.addToBounds(this.p.getX(), this.p.getY(), p.getZ());
-                bounds.addToBounds(this.p.getX() + w, this.p.getY() - h,
-                    p.getZ());
+                case ATTACHMENT_TOP_LEFT:
+                    bounds.addToBounds(this.p.getX(), this.p.getY(), p.getZ());
+                    bounds.addToBounds(this.p.getX() + w, this.p.getY() - h,
+                                       p.getZ());
 
-                break;
+                    break;
 
-            case ATTACHMENT_TOP_CENTER:
-                bounds.addToBounds(this.p.getX() + (w / 2), this.p.getY(),
-                    p.getZ());
-                bounds.addToBounds(this.p.getX() - (w / 2), this.p.getY() - h,
-                    p.getZ());
+                case ATTACHMENT_TOP_CENTER:
+                    bounds.addToBounds(this.p.getX() + (w / 2), this.p.getY(),
+                                       p.getZ());
+                    bounds.addToBounds(this.p.getX() - (w / 2), this.p.getY() - h,
+                                       p.getZ());
 
-                break;
+                    break;
 
-            case ATTACHMENT_TOP_RIGHT:
-                bounds.addToBounds(this.p.getX(), this.p.getY(), p.getZ());
-                bounds.addToBounds(this.p.getX() - w, this.p.getY() - h,
-                    p.getZ());
+                case ATTACHMENT_TOP_RIGHT:
+                    bounds.addToBounds(this.p.getX(), this.p.getY(), p.getZ());
+                    bounds.addToBounds(this.p.getX() - w, this.p.getY() - h,
+                                       p.getZ());
 
-                break;
+                    break;
             }
         } else {
             bounds.setValid(false);
